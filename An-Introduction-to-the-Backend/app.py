267b52723873595558eef60b01fd0a935214808a
perry0226 <160164707+perry0226@users.noreply.github.com>
2024-10-02 20:37:51 -0700

    commiy

--- An-Introduction-to-the-Backend/app.py
+++ An-Introduction-to-the-Backend/app.py
@@ -1,5 +1,46 @@
 # API HERE
+from os import name
+from flask import Flask, jsonify, request
 
+app= Flask(__name__)
+
+@app.get('/user')
+def get_all_user():
+    user_data = users_db
+    if user_data:
+        return jsonify(user_data), 200
+    else:
+        return { "response": "That resource could not be found!"}, 404
+
+@app.post('/getuser')
+def create_new_user():
+    input= request["name"]
+    email=input["email"]
+    user_id=create_user(users_db, name, email)
+
+    user=get_user(user_id)
+
+
+    if user:
+        return jsonify(get_user(user_id)), 200
+    else:
+        return{ "Response": "Failed to create a new user!" }, 404   
+    
+    @app.get('/user/<int: user_id>')
+    def get_user_by_id(user_id):
+        user_data = get_user(user_id)
+
+        if user_data:
+            return jsonify(user_data), 200
+        else:
+            return { " response ": "A user with that ID was not found!" }, 400
+        
+    @app.delete('/user/<int:user_id>')
+    def delete_user(user_id):
+        if delete_user(user_id):
+            return { "Responce": " A user with id: {user_id} was deleted!"}, 200
+        else:
+            return {" response": f"Failed to delete user with id:{user_id}"}, 400
 # get    /users
 # post   /user
 # get    /user/123
